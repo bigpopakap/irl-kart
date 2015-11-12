81c9a470ee31055d116c699237e7558b47d59498
Kapil Easwar <bigpopakap@gmail.com>
2015-11-11 20:44:48 -0800

    fixed timing issue when engine was started before world

--- irl-util/src/java/irl/util/reactiveio/Pipe.java
+++ irl-util/src/java/irl/util/reactiveio/Pipe.java
@@ -2,7 +2,7 @@ package irl.util.reactiveio;
 
 import rx.Observable;
 import rx.Observer;
-import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 import rx.subjects.Subject;
 
 /**
@@ -16,7 +16,7 @@ public class Pipe<T> {
     private final Subject<T, T> pipe;
 
     public Pipe() {
-        pipe = PublishSubject.<T>create().toSerialized();
+        pipe = ReplaySubject.<T>create().toSerialized();
 
         //make sure the pipe never completes
         mergeIn(Observable.<T>never());
