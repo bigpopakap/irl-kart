0c12e8c0c2ea598b3ea91d33526e2ed28e3aaeb9
Kapil Easwar <bigpopakap@gmail.com>
2015-11-15 14:46:26 -0800

    clean up imports

--- irl-kart/src/java/irl/kart/entities/items/ShellItem.java
+++ irl-kart/src/java/irl/kart/entities/items/ShellItem.java
@@ -1,7 +1,6 @@
 package irl.kart.entities.items;
 
 import irl.fw.engine.entity.Entity;
-import irl.fw.engine.entity.factory.EntityConfig;
 import irl.fw.engine.entity.factory.EntityFactory;
 import irl.fw.engine.entity.state.EntityState;
 import irl.fw.engine.entity.state.EntityStateBuilder;
@@ -14,7 +13,6 @@ import irl.fw.engine.geometry.Vector2D;
 import irl.kart.entities.Kart;
 import irl.kart.entities.items.actions.holdableitem.HoldableItem;
 import irl.kart.entities.items.actions.holdableitem.HoldableItemAdaptor;
-import irl.kart.entities.weapons.Banana;
 import irl.kart.entities.weapons.Shell;
 import irl.kart.entities.items.actions.itemuser.ItemUser;
 import irl.util.callbacks.Callback;
