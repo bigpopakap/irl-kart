06dc7e891f001ba15505aaee2c45f32d27711b56
Kapil Easwar <bigpopakap@gmail.com>
2015-11-16 13:25:05 -0800

    added the ability to specify multiple keys for the same function in the key mappings

--- irl-kart/src/java/irl/kart/beacon/impl/swing/SwingKeyMapping.java
+++ irl-kart/src/java/irl/kart/beacon/impl/swing/SwingKeyMapping.java
@@ -1,7 +1,9 @@
 package irl.kart.beacon.impl.swing;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,14 +19,14 @@ class SwingKeyMapping {
             .map(SwingKartInput.LEFT, KeyEvent.VK_A)
             .map(SwingKartInput.DOWN, KeyEvent.VK_S)
             .map(SwingKartInput.RIGHT, KeyEvent.VK_D)
-            .map(SwingKartInput.FIRE, KeyEvent.VK_E)
+            .map(SwingKartInput.FIRE, KeyEvent.VK_X, KeyEvent.VK_Z)
             .build();
     public static final SwingKeyMapping UHJK = new Builder()
             .map(SwingKartInput.UP, KeyEvent.VK_U)
             .map(SwingKartInput.LEFT, KeyEvent.VK_H)
             .map(SwingKartInput.DOWN, KeyEvent.VK_J)
             .map(SwingKartInput.RIGHT, KeyEvent.VK_K)
-            .map(SwingKartInput.FIRE, KeyEvent.VK_I)
+            .map(SwingKartInput.FIRE, KeyEvent.VK_N, KeyEvent.VK_M)
             .build();
     public static final SwingKeyMapping ARROWS = new Builder()
             .map(SwingKartInput.UP, KeyEvent.VK_UP)
@@ -35,14 +37,16 @@ class SwingKeyMapping {
             .build();
 
     private final Map<Integer, SwingKartInput> keyCodeToKartInput;
-    private final Map<SwingKartInput, Integer> kartInputToKeyCode;
+    private final Map<SwingKartInput, List<Integer>> kartInputToKeyCode;
 
-    private SwingKeyMapping(Map<SwingKartInput, Integer> keyMap) {
+    private SwingKeyMapping(Map<SwingKartInput, List<Integer>> keyMap) {
         this.kartInputToKeyCode = new HashMap<>(keyMap);
 
         this.keyCodeToKartInput = new HashMap<>();
-        this.kartInputToKeyCode.forEach((kartInput, keyCode) -> {
-            this.keyCodeToKartInput.put(keyCode, kartInput);
+        this.kartInputToKeyCode.forEach((kartInput, keyCodes) -> {
+            keyCodes.forEach(keyCode -> {
+                this.keyCodeToKartInput.put(keyCode, kartInput);
+            });
         });
 
         //check that all inputs are in the map
@@ -65,19 +69,31 @@ class SwingKeyMapping {
     }
 
     public int fromKartInput(SwingKartInput input) {
-        return kartInputToKeyCode.get(input);
+        //just return the first, since any will do
+        return kartInputToKeyCode.get(input).get(0);
     }
 
     private static class Builder {
 
-        private Map<SwingKartInput, Integer> keyMap;
+        private Map<SwingKartInput, List<Integer>> keyMap;
 
         public Builder() {
             keyMap = new HashMap<>();
         }
 
-        public Builder map(SwingKartInput input, int keyCode) {
-            keyMap.put(input, keyCode);
+        public Builder map(SwingKartInput input, int... keyCodes) {
+            List<Integer> keyCodeList = new ArrayList<>();
+            for (int keyCode : keyCodes) {
+                keyCodeList.add(keyCode);
+            }
+            return map(input, keyCodeList);
+        }
+
+        private Builder map(SwingKartInput input, List<Integer> keyCodes) {
+            if (!keyMap.containsKey(input)) {
+                keyMap.put(input, new ArrayList<>());
+            }
+            keyMap.get(input).addAll(keyCodes);
             return this;
         }
 
