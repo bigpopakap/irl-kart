42e980134f08b6dc878811eae512d404fb88109e
Kapil Easwar <bigpopakap@gmail.com>
2017-07-14 09:35:04 -1000

    Added the simple server to post a JSON representation of the world

--- irl-fw-engine/src/java/irl/fw/engine/graphics/irl/fw/engine/graphics/impl/server/ServerRenderer.java
+++ irl-fw-engine/src/java/irl/fw/engine/graphics/irl/fw/engine/graphics/impl/server/ServerRenderer.java
@@ -1,10 +1,17 @@
 package irl.fw.engine.graphics.irl.fw.engine.graphics.impl.server;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import irl.fw.engine.graphics.Renderer;
 import irl.fw.engine.world.World;
 import irl.util.callbacks.Callback;
 import irl.util.callbacks.Callbacks;
 import irl.util.concurrent.StoppableRunnable;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 /**
  * TODO bigpopakap Javadoc this class
@@ -14,12 +21,24 @@ import irl.util.concurrent.StoppableRunnable;
  */
 public class ServerRenderer implements Renderer, StoppableRunnable {
 
+    private static final String PATH = "/world";
+    private static final int PORT = 8080;
+
     private boolean isStopped = false;
     private final Callbacks onStop = new Callbacks();
+
+    private final HttpServer server;
+    private String serverResponse = "";
+
+    public ServerRenderer() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server.createContext(PATH, new WorldHandler());
+    }
 
     @Override
     public void stop() {
         if (!isStopped()) {
+            server.stop(0);
             isStopped = true;
             onStop.run();
         }
@@ -37,14 +56,28 @@ public class ServerRenderer implements Renderer, StoppableRunnable {
 
     @Override
     public void run() {
-        // do nothing, yet
-        // TODO run a server
+        server.start();
     }
 
     @Override
     public void render(World world, long timeSinceLastUpdate) {
-        // TODO use Gson
-        System.out.println(world.toJSON());
+        synchronized (serverResponse) {
+            serverResponse = world.toJSON();
+        }
+    }
+
+    private class WorldHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            synchronized (serverResponse) {
+                t.sendResponseHeaders(200, serverResponse.getBytes().length);
+                OutputStream os = t.getResponseBody();
+                os.write(serverResponse.getBytes());
+                os.close();
+            }
+        }
+
     }
 
 }
