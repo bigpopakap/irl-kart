818c4d5bb2c7f559ceedb6590a880ccc5ebae4aa
Kapil Easwar <bigpopakap@gmail.com>
2017-07-14 13:49:46 -1000

    Open the browser to the server URL to see the JSON

--- irl-fw-engine/src/java/irl/fw/engine/graphics/irl/fw/engine/graphics/impl/server/ServerRenderer.java
+++ irl-fw-engine/src/java/irl/fw/engine/graphics/irl/fw/engine/graphics/impl/server/ServerRenderer.java
@@ -9,9 +9,12 @@ import irl.util.callbacks.Callback;
 import irl.util.callbacks.Callbacks;
 import irl.util.concurrent.StoppableRunnable;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * TODO bigpopakap Javadoc this class
@@ -23,12 +26,15 @@ public class ServerRenderer implements Renderer, StoppableRunnable {
 
     private static final String PATH = "/world";
     private static final int PORT = 8080;
+    private static final String URL = "http://localhost:" + PORT + PATH;
 
     private boolean isStopped = false;
     private final Callbacks onStop = new Callbacks();
 
     private final HttpServer server;
-    private String serverResponse = "";
+    // NOTE: this is an array so that the reference doesn't change
+    // and we can therefore do a synchronized() on this variable
+    private final String[] serverResponse = new String[] { "" };
 
     public ServerRenderer() throws IOException {
         server = HttpServer.create(new InetSocketAddress(PORT), 0);
@@ -54,15 +60,32 @@ public class ServerRenderer implements Renderer, StoppableRunnable {
         return onStop.add(callback);
     }
 
+    private String getServerResponse() {
+        return serverResponse[0];
+    }
+
+    private void setServerResponse(String serverResponse) {
+        this.serverResponse[0] = serverResponse;
+    }
+
     @Override
     public void run() {
         server.start();
+
+        // Open the browser to this tab
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI(URL));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void render(World world, long timeSinceLastUpdate) {
         synchronized (serverResponse) {
-            serverResponse = world.toJSON();
+            setServerResponse(world.toJSON());
         }
     }
 
@@ -71,9 +94,9 @@ public class ServerRenderer implements Renderer, StoppableRunnable {
         @Override
         public void handle(HttpExchange t) throws IOException {
             synchronized (serverResponse) {
-                t.sendResponseHeaders(200, serverResponse.getBytes().length);
+                t.sendResponseHeaders(200, getServerResponse().getBytes().length);
                 OutputStream os = t.getResponseBody();
-                os.write(serverResponse.getBytes());
+                os.write(getServerResponse().getBytes());
                 os.close();
             }
         }
